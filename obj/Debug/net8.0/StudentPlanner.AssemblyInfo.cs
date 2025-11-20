722a287f7463f6ef6c3d85530ac4969adea7049f
Dave2317 <harshilsdave2314@gmail.com>
2025-11-19 21:05:01 -0800

    Theme state changed

--- obj/Debug/net8.0/StudentPlanner.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3f3cfcab1e9c7d860f2bcbdd29d60f06a3a940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b90d7b41ce6d8cf4fc4c1bc102d064007805d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
